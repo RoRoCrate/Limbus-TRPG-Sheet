d22664067fcc6a3be9d66561fced147f06f302a4
正岡子規 <roro.crate@gmail.com>
2025-12-07 22:44:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         'mental': { 
             name: '精神', 
-            url: '（https://docs.google.com/spreadsheets/d/1N5WzWNf4UbBxeAwC_TjJuNrGFN2l4n9Qjweb-9VTLTU/edit?usp=sharing）' 
+            url: '（https://docs.google.com/spreadsheets/d/e/2PACX-1vTq-eD845A-_cecFEMhZmiFEMzQ2P6iokgZs-A2rAMPM8NjcFlwE56H5kEMIGYXW30NlCeVathdxYWE/pub?gid=0&single=true&output=tsv）' 
         },
         'ego': { 
             name: 'E.G.O', 
@@ -1328,3 +1328,4 @@ document.addEventListener('DOMContentLoaded', () => {
         initialize();
     });
 });
+
